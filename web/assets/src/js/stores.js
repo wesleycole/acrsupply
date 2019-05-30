ba3f2f1663de2163802121e112c3bc84d36d7d00
Wes Cole <wesley.cole87@gmail.com>
2019-05-29 20:53:34 -0400

    site updates

--- web/assets/src/js/stores.js
+++ web/assets/src/js/stores.js
@@ -1,56 +1,62 @@
 export const stores = [
   {
-    title: 'ACR Supply Supply Distribution Center',
-    address: '4040 S. Alston Ave, Durham, NC 27713',
-    phone: '(919) 765 - 8081',
+    title: "ACR Supply Supply Distribution Center",
+    address: "4040 S. Alston Ave, Durham, NC 27713",
+    phone: "(919) 765 - 8081",
     position: { lat: 35.9187832, lng: -78.88911029999997 }
   },
   {
-    title: 'ACR Supply Durham',
-    address: '2719 Hillsborough Rd, Durham, NC 27705',
-    phone: '(919) 286 - 2228',
+    title: "ACR Supply Durham",
+    address: "2719 Hillsborough Rd, Durham, NC 27705",
+    phone: "(919) 286 - 2228",
     position: { lat: 36.01227739999999, lng: -78.9316978 }
   },
   {
-    title: 'ACR Supply Carrboro',
-    address: '101 Barnes St., Carrboro, NC 27510',
-    phone: '(919) 929 - 2099',
+    title: "ACR Supply Carrboro",
+    address: "101 Barnes St., Carrboro, NC 27510",
+    phone: "(919) 929 - 2099",
     position: { lat: 35.9083652, lng: -79.07904189999999 }
   },
   {
-    title: 'ACR Supply Burlington',
-    address: '2352 W. Hanford Rd., Burlington, NC 27215',
-    phone: '(336) 227 - 5000',
+    title: "ACR Supply Burlington",
+    address: "2352 W. Hanford Rd., Burlington, NC 27215",
+    phone: "(336) 227 - 5000",
     position: { lat: 36.0605812, lng: -79.4372502 }
   },
   {
-    title: 'ACR Supply Greensboro',
-    address: '2201 Binford St., Greensboro, NC 27407',
-    phone: '(336) 854 - 5800',
+    title: "ACR Supply Greensboro",
+    address: "2201 Binford St., Greensboro, NC 27407",
+    phone: "(336) 854 - 5800",
     position: { lat: 36.05970929999999, lng: -79.83266220000002 }
   },
   {
-    title: 'ACR Supply Raleigh',
-    address: '2416 Alwin Ct., Raleigh, NC 27604',
-    phone: '(919) 861 - 2277',
+    title: "ACR Supply Raleigh",
+    address: "2416 Alwin Ct., Raleigh, NC 27604",
+    phone: "(919) 861 - 2277",
     position: { lat: 35.81185060000001, lng: -78.61677179999998 }
   },
   {
-    title: 'ACR Supply Wilmington',
-    address: '3207 Burnt Mill Dr., Wilmington, NC 28403',
-    phone: '(919) 442 - 3303',
+    title: "ACR Supply Wilmington",
+    address: "3207 Burnt Mill Dr., Wilmington, NC 28403",
+    phone: "(919) 442 - 3303",
     position: { lat: 34.229813, lng: -77.9023899 }
   },
   {
-    title: 'ACR Supply Winston-Salem',
-    address: '1735 Link Rd., Winston-Salem, NC 27103',
-    phone: '(336) 831 - 0505',
+    title: "ACR Supply Winston-Salem",
+    address: "1735 Link Rd., Winston-Salem, NC 27103",
+    phone: "(336) 831 - 0505",
     position: { lat: 36.0706427, lng: -80.26310219999999 }
   },
   {
-    title: 'ACR Supply Winterville',
-    address: '4125 Bayswater Rd., Winterville, NC 28590',
-    phone: '(252) 754 - 2653',
+    title: "ACR Supply Winterville",
+    address: "4125 Bayswater Rd., Winterville, NC 28590",
+    phone: "(252) 754 - 2653",
     position: { lat: 35.553177, lng: -77.37196510000001 }
   },
+  {
+    title: "Sales and Marketing Office",
+    address: "200 Seneca Road, Greensboro NC, 27406",
+    position: { lat: 36.0359615, lng: -79.7890453 }
+  }
 ];
+
